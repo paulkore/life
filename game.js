130e8aa5ee65ab15c4ac9b33bf451d089b687b1b
paulkore <paulkore@outlook.com>
2014-09-07 16:10:10 -0400

    add capability to define known shapes
    - rename function add() to addCell()
    - add function addShape()
    - define several known shapes, and add them to the game
    - comment out old cell-adding code
    - more stylistic changes.... use 2 characters horizontally per cell, to
    make things more even

--- game.js
+++ game.js
@@ -4,9 +4,7 @@ var Game = function() {
   var gamestateFromSet = function(set) {
     var gamestate = {};    
 
-
     for (var cell in set) {
-
       var a = neighboursOf(cell);
       a.forEach(function(n) {
         if (!gamestate[n]) gamestate[n] = 0
@@ -22,10 +20,30 @@ var Game = function() {
     return [[x-1, y-1], [x-1, y], [x-1, y+1], [x, y-1], [x, y+1], [x+1, y-1], [x+1, y], [x+1, y+1]]   
   }
 
-  this.add = function(cell) {
+  this.addCell = function(cell) {
     this.set[cell] = true;
   }
 
+  this.addShape = function(shape, offset_x, offset_y) {
+	if (!offset_x)
+		offset_x = 0;
+		
+	if (!offset_y)
+		offset_y = 0;
+	
+	for(var y = 0; y < shape.length; y++) {
+		var row_y = shape[y];
+		for(var x = 0; x < row_y.length; x++) {
+			if (row_y[x] === 1) {
+				var cell_x = offset_x + x;
+				var cell_y = offset_y + y;
+				this.addCell([cell_x, cell_y]);
+			}					
+		}
+	}
+	
+  }
+  
   this.isAlive = function(cell) {
     return this.set[cell];
   }
